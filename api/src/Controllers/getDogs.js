d9e38b986f2d263379f2e01d458176b4ce6c9c80
Farith Romero <farithromero@gmail.com>
2023-07-19 19:43:41 -0500

    Aplicación funcionando con corrección de llamado de data en DB

--- api/src/Controllers/getDogs.js
+++ api/src/Controllers/getDogs.js
@@ -5,83 +5,103 @@ const {URL} = process.env;
 const capitalizeString = require('../utils/validations.js');
 
 const getDogsAndQuery = async(req, res) => { 
-    const name = req.query.name;
+  const name = req.query.name;
 
-    if(name !== undefined){//si recibe un perro por query
-      const dogWanted = capitalizeString(name);//transformelo a un formato valido
+  if(name !== undefined){//si recibe un perro por query
+    const dogWanted = capitalizeString(name);//transformelo a un formato valido
 
-      try { 
-        const {data} = await axios(URL);  // haga un llamado a la API
-        const findApi = data.find( perro => perro.name.toString() == dogWanted );// busque el perro  en la api
-        if(findApi === undefined){  //si el perro NO está en la API
+    try { 
+      const {data} = await axios(URL);  // haga un llamado a la API
+      const findApi = data.find( perro => perro.name.toString() == dogWanted );// busque el perro  en la api
+      
+      if(findApi === undefined){  //si el perro NO está en la API
+        
         console.log("El perro no se encuentra en la API")
+        const findDb = await Dog.findOne({ where: { nombre: dogWanted } });//busquelo en la base de datos
+        let filterTemperaments = (await Dogs_Temperaments.findAll({ where: { DogId: findDb.id } })).map((t) => t.TemperamentId);;
+        let temperamentOne = (await Temperament.findAll({ where: { id: filterTemperaments[0] } })).map((t) => t.temperamento);
+        let temperamentTwo = (await Temperament.findAll({ where: { id: filterTemperaments[1] } })).map((t) => t.temperamento);
+        let temperamentos = (temperamentOne.concat(temperamentTwo)).join(", ");
 
-          const findDb = await Dog.findOne({ where: { nombre: dogWanted } });//busquelo en la base de datos
-          let filterTemperaments = (await Dogs_Temperaments.findAll({ where: { DogId: findDb.id } })).map((t) => t.TemperamentId);;
- 
-          let temperamentOne = (await Temperament.findAll({ where: { id: filterTemperaments[0] } })).map((t) => t.temperamento);
-          let temperamentTwo = (await Temperament.findAll({ where: { id: filterTemperaments[1] } })).map((t) => t.temperamento);
-          let temperamentos = (temperamentOne.concat(temperamentTwo)).join(", ");
-
-          if(findDb){
-           console.log("El perro se encuentra en la DataBase") 
-           return res.status(200).send(
-            {
-              id: findDb.id,
-              imagen: findDb.imagen,
-              nombre: findDb.nombre,       
-              peso: findDb.peso,
-              temperamentos: temperamentos,
-              Origen: "DataBase",
-          });
-          }else{
-            return res.status(400).send("El perro no se encuentra en la base de datos");
-          };
-        }else{ //si el perro está en la API traigalo
-            console.log("El perro está en la API");
+        if(findDb){
+          console.log("El perro se encuentra en la DataBase") 
           return res.status(200).send({
-              id: findApi.id,
-              imagen: findApi.image.url,
-              nombre: findApi.name,
-              altura: findApi.height.metric, 
-              peso: findApi.weight.imperial,
-              temperamentos: findApi.temperament,
-              años_de_vida: findApi.life_span,
-              Origen: "Api"
+            id: findDb.id,
+            imagen: findDb.imagen,
+            nombre: findDb.nombre,       
+            peso: findDb.peso,
+            temperamentos: temperamentos,
+            Origen: "DataBase",
           });
+        }else{
+          return res.status(400).send("El perro no se encuentra en la base de datos");
         };
-        } catch (error) {
-          res.status(400).send(error.message);
-        }; 
-      };  
-      if(name === undefined){  //si no se hizo un llamado por query
-        try {
-          await axios(URL) //llame a la API
-          .then(response => {
-              const data = response.data;
-              let razas = [];
-              data.forEach(dog => { // cree a todas las razas dentro de un array
-                let objApi = {
-                  id: dog.id,
-                  Imagen: dog.image.url,
-                  Nombre: dog.name,
-                  Temperamentos: dog.temperament,
-                  Peso: dog.weight.imperial,
-                  Origen: "Api"
-                }
-                razas.push(objApi)
-              });  
-          res.status(200).send(razas);// retorne las razas
-          });  
-        } catch (error) {
-          res.status(400).send(error.message);
-        };
+      
+      }else{ //si el perro está en la API traigalo
+        console.log("El perro está en la API");
+        return res.status(200).send({
+          id: findApi.id,
+          imagen: findApi.image.url,
+          nombre: findApi.name,
+          altura: findApi.height.metric, 
+          peso: findApi.weight.imperial,
+          temperamentos: findApi.temperament,
+          años_de_vida: findApi.life_span,
+          Origen: "Api"
+        });
       };
+    } catch (error) {
+      res.status(400).send(error.message);
+    }; 
+  };  
+    
+  if(name === undefined){  //si no se hizo un llamado por query
+    let razas = [];
+    try {
+      const {data} = await axios(URL);  //traer todos los perrros de la API
+      data.forEach(dog => { //Empuje cada perro al array de razas
+        let objApi = {
+          id: dog.id,
+          Imagen: dog.image.url,
+          Nombre: dog.name,
+          Temperamentos: dog.temperament,
+          Peso: dog.weight.imperial,
+          Origen: "Api"
+        };
+      razas.push(objApi);
+      });  
+      res.status(200).send(razas);// retorne las razas
+    } catch (error) {
+      res.status(400).send(error.message);
+    };
   };
-  
+};
+
   
 module.exports ={
     getDogsAndQuery,
 } 
 
-  
+// try {
+//   await axios(URL) //llame a la API
+//   .then(response => {
+//     const data = response.data;
+//     let razas = [];
+//       data.forEach(dog => { // cree a todas las razas dentro de un array
+//         let objApi = {
+//           id: dog.id,
+//           Imagen: dog.image.url,
+//           Nombre: dog.name,
+//           Temperamentos: dog.temperament,
+//           Peso: dog.weight.imperial,
+//           Origen: "Api"
+//         }
+//         razas.push(objApi)
+//       });  
+//     res.status(200).send(razas);// retorne las razas
+//   });  
+// } catch (error) {
+//   res.status(400).send(error.message);
+// };
+// };
+// };
